021407bd8f3e25dd9f14cafc4bddf8641b16b7d8
emptyasmybrainn <bidyutbikashboruah999@gmail.com>
2023-04-14 23:18:51 +0530

    prompt scrool

---
+++ main.py
@@ -0,0 +1,12 @@
+import time
+import os
+
+def scroll_prompt(prompt, speed=0.1):
+    while True:
+        os.system('clear')  # Clear the console
+        print(prompt)
+        time.sleep(speed)  # Wait for a few seconds
+        prompt = prompt[1:] + prompt[0]  # Shift the prompt to the left
+
+# Example usage:
+scroll_prompt(input(str()))
